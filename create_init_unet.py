4b1a976ffcdab616c3a1893bfdbcb06ba569346a
IzumiSatoshi <izumisatoshi05@gmail.com>
2023-01-01 16:00:49 +0900

    delete create init unet from torch model

--- create_init_unet.py
+++ create_init_unet.py
@@ -46,39 +46,3 @@ def create_init_sketch2img_pipe(pipe, save_pipe_path, add_channels_num):
 
     # save
     pipe.save_pretrained(save_pipe_path)
-
-
-def from_torch_model():
-    device = "cpu"
-    base_model_path = ""
-    save_path = ""
-
-    base_model = torch.load(torch)
-
-    state_dict = base_model.state_dict()
-    original_weight = state_dict["conv_in.weight"]
-    s = original_weight.shape
-    zero_weight = torch.zeros((s[0], 1, s[2], s[3]))
-    new_weight = torch.concat([original_weight, zero_weight], dim=1)
-
-    state_dict["conv_in.weight"] = new_weight
-
-    new_model = UNet2DModel(
-        sample_size=28,  # the target image resolution
-        in_channels=2,  # the number of input channels, 3 for RGB images
-        out_channels=1,  # the number of output channels
-        layers_per_block=2,  # how many ResNet layers to use per UNet block
-        block_out_channels=(32, 64),  # More channels -> more parameters
-        down_block_types=(
-            "DownBlock2D",  # a regular ResNet downsampling block
-            "AttnDownBlock2D",  # a ResNet downsampling block with spatial self-attention
-        ),
-        up_block_types=(
-            "AttnUpBlock2D",  # a ResNet upsampling block with spatial self-attention
-            "UpBlock2D",  # a regular ResNet upsampling block
-        ),
-    )
-
-    new_model.load_state_dict(state_dict)
-
-    torch.save(new_model, save_path)
