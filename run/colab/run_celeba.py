853c052bd71e1fbed5419eef71f996c9a6ad2945
IzumiSatoshi <izumisatoshi05@gmail.com>
2023-01-03 16:49:01 +0900

    add colab run script

---
+++ run/colab/run_celeba.py
@@ -0,0 +1,38 @@
+import sys
+import random
+
+sys.path.append("./src")  # running on /sketch2img
+from datasets_sketch2img import CelebaDataset
+from trainer_ddpm_sketch2img import Trainer
+
+project_name = "sketch2img_celeba_ddpm"
+run_name = "1epoch" + "_" + str(random.randint(0, 1000))
+data_dir = "../data2/celeba_hq_256"
+pretrained_model_path = (
+    "/content/drive/MyDrive/Project/sketch2img/model/init_s2i_celeba"
+)
+save_path = "/content/drive/MyDrive/Project/sketch2img/model/" + run_name
+
+train_dataset_rate = 1
+image_log_steps = 500
+num_epochs = 1
+batch_size = 6
+grad_accumulation_steps = 1
+lr = 1e-3
+
+dataset = CelebaDataset(data_dir)
+trainer = Trainer(
+    pretrained_model_name_or_path=pretrained_model_path,
+    dataset=dataset,
+    save_path=save_path,
+    num_epochs=num_epochs,
+    batch_size=batch_size,
+    lr=lr,
+    grad_accumulation_steps=grad_accumulation_steps,
+    train_dataset_rate=train_dataset_rate,
+    project_name=project_name,
+    run_name=run_name,
+    image_log_steps=image_log_steps,
+)
+
+trainer.train()
